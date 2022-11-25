da53c07ae6cd929464dab21bc75f6c8e98e99fed
Lívio Lopes <antonio.lvio760@gmail.com>
2022-11-25 18:10:27 -0300

    corrigindo planetCard

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -6,8 +6,8 @@ class PlanetCard extends Component {
     const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
-        <img src={ planetImage } alt={ planetName } />
         <p data-testid="planet-name">{planetName}</p>
+        <img src={ planetImage } alt={ planetName } />
       </div>
     );
   }
