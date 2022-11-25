9abdb60d84b319a5db06b2add68e13ef0d5c4313
Lívio Lopes <antonio.lvio760@gmail.com>
2022-11-25 15:18:18 -0300

    requisito 3 e 4 resolvidos

---
+++ src/components/Title.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+  render() {
+    const { headline } = this.props;
+    return (
+      <h2>
+        {headline}
+      </h2>
+    );
+  }
+}
+
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
+
+export default Title;
